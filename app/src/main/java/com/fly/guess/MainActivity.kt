673e1b8ba57f0929c9062f2397979abf802a774d
MuHongWeiWei <4a490094@stust.edu.tw>
2019-04-12 10:00:33 +0800

    function design, toast, dialog

--- app/src/main/java/com/fly/guess/MainActivity.kt
+++ app/src/main/java/com/fly/guess/MainActivity.kt
@@ -1,12 +1,37 @@
 package com.fly.guess
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private val secretNumber = SecretNumber()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+
+
+    fun check(view : View) {
+        val number = et_number.text.toString().trim().toInt()
+        println(secretNumber.secret)
+        val result = secretNumber.validate(number)
+        var message = "Yes, you got it!"
+        if (result >  0) {
+            message = "smaller"
+        } else if (result < 0) {
+            message = "bigger"
+        }
+//        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+        AlertDialog.Builder(this)
+            .setTitle("Message")
+            .setMessage(message)
+            .setPositiveButton("ok", null)
+            .show()
+    }
+
 }
--- app/src/main/java/com/fly/guess/SecretNumber.kt
+++ app/src/main/java/com/fly/guess/SecretNumber.kt
@@ -1,5 +1,21 @@
 package com.fly.guess
 
-class SecretNumber{
+import java.util.*
 
+class SecretNumber{
+    val secret = Random().nextInt(10) + 1
+    var count = 0
+
+    fun validate(number : Int) : Int {
+        count++
+        return number - secret
+    }
+}
+
+fun main() {
+    val secretNumber = SecretNumber()
+    val secret = secretNumber.secret
+    println(secret)
+    var number = secretNumber.validate(5)
+    println(number)
 }
